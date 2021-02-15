0910e9f9a1dc42a3fd2114be50f34478dbef32d4
gigagibi <gii01@mail.ru>
2021-02-15 23:38:17 +0300

    1

--- lesson11.py
+++
@@ -1,4 +0,0 @@
-import math
-
-def f11(x, y, z):
-    return (6*z**2 - 9*z**7 + (61*y**6-y**7 + 4)**(0.5) + (math.sin(y) - 75*y**5)**(0.5))
--- lesson13.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def f13(n):
-    sum1, sum2 = 0, 0
-    for i in range(1, n+1):
-        sum1 += (6*i**2-math.cos(i))
-        sum2 += (i**7+86*i)
-    return sum1 - sum2/98
